99ce3ebee4c6b012b40aa71323748abf64ad4f5a
tan1155 <tan1155@yahoo.com>
2022-09-16 20:10:12 +0800

    20220916 pub turn on client.loop_start

--- mqtt_pub_from_patient_client.py
+++ mqtt_pub_from_patient_client.py
@@ -104,9 +104,9 @@ def publish(client, fileName, messageText):
 
 # Entry Point for Publishing
 def run(paramFilepath, paramMessageText):
-    client = None
+    #client = None
     client = connect_mqtt()
-    #client.loop_start()
+    client.loop_start()
     publish(client, paramFilepath, paramMessageText)
     #    if status = client.disconnect
     #    return status
