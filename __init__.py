d91d3db9f97384688f0d9b9a11ba56a8374549a7
tan1155 <tan1155@yahoo.com>
2022-09-19 17:46:52 +0800

    20220919 Update init

--- __init__.py
+++ __init__.py
@@ -105,14 +105,14 @@ class NurseAssitant(MycroftSkill):
 
     def converse(self, utterances, lang="en-us"):
         if self.dictating:
-            #if utterances and self.voc_match(utterances,"assitant.nurse"):
-            self.log.info("Dictating: " + utterances)
-            self.dictation_stack.append(utterances)
-            return True
-            #else:
-                #self.remove_context("DictationKeyword")
-
-                #return False
+            if utterances:
+                self.log.info("Dictating: " + utterances)
+                self.dictation_stack.append(utterances)
+                return True
+            else:
+                self.remove_context("DictationKeyword")
+                publish_data(None,None,4)
+                return False
         else:
             self.remove_context("DictationKeyword")
             publish_data(None,None,4)
